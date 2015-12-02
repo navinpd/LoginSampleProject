8db11570ba67dcda545664635eb90dc1c706dfca
Navin prasad <navin.pd@gmail.com>
2015-12-02 08:14:57 +0800

    Update commit to change login xml

--- app/src/main/java/com/example/navin/loginsampleproject/Activity/LoginActivity.java
+++ app/src/main/java/com/example/navin/loginsampleproject/Activity/LoginActivity.java
@@ -3,11 +3,10 @@ package com.example.navin.loginsampleproject.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -27,6 +26,7 @@ public class LoginActivity extends BaseActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setupViews();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +45,24 @@ public class LoginActivity extends BaseActivity {
         gmailImage = (ImageView) findViewById(R.id.gmail_item);
         mailIdEdit = (EditText) findViewById(R.id.mail_id);
         passwordEdit = (EditText) findViewById(R.id.password);
+    }
+
+    private void clickView(View v) {
+        int id = v.getId();
+
+        switch (id) {
+            case R.id.fb_button:
+                break;
+
+            case R.id.gmail_item:
+                break;
+
+            case R.id.pintrest_item:
+                break;
+
+            case R.id.linkedin_item:
+                break;
+        }
     }
 
     @Override
